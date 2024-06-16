2e2b0627f371534c62b0df83ab895b2d20a9e02b
dennisnderitu254 <dknderitu@gmail.com>
2024-06-16 19:38:41 +0300

    removing switch library from app file - react dom

--- employeemgtsystem/src/App.tsx
+++ employeemgtsystem/src/App.tsx
@@ -1,6 +1,6 @@
 // src/App.tsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './LandingPage';
 import LoginPage from './LoginPage';
 
@@ -8,13 +8,13 @@ const App: React.FC = () => {
   return (
     <Router>
       <div className="App">
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route path="/login" component={LoginPage} />
-          <Route path="/login/admin" render={() => <div>Admin Login Page</div>} />
-          <Route path="/login/manager" render={() => <div>Manager Login Page</div>} />
-          <Route path="/login/employee" render={() => <div>Employee Login Page</div>} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/login/admin" element={<div>Admin Login Page</div>} />
+          <Route path="/login/manager" element={<div>Manager Login Page</div>} />
+          <Route path="/login/employee" element={<div>Employee Login Page</div>} />
+        </Routes>
       </div>
     </Router>
   );
